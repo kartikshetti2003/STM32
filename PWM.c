3cda25a914180f0e883462f4692a2d5e3940ccc5
Kartik Shetti <109369540+kartikshetti2003@users.noreply.github.com>
2025-05-02 10:00:02 +0530

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -3,10 +3,10 @@
 #include "pwm.h"
 
 TIM_HandleTypeDef htim1;
-volatile uint32_t pwm_duty_cycle = 0;  // Variable to hold the current duty cycle
+volatile uint32_t pwm_duty_cycle = 0;  
 
 static void MX_TIM1_Init(void);
-void Error_Handler(void);  // Declare Error_Handler function
+void Error_Handler(void); 
 
 void blink_led_pwm(void)
 {
@@ -25,16 +25,16 @@ static void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 72 - 1;  // Prescaler set to 72 to get 1 MHz timer clock
+  htim1.Init.Prescaler = 72 - 1;  
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 100 - 1;  // Period set to 100 for 10 kHz frequency
+  htim1.Init.Period = 100 - 1; 
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 
   if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
   {
-    Error_Handler();  // Call Error_Handler on error
+    Error_Handler();
   }
 
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
@@ -42,11 +42,11 @@ static void MX_TIM1_Init(void)
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
   {
-    Error_Handler();  // Call Error_Handler on error
+    Error_Handler();
   }
 
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = pwm_duty_cycle;  // Initial duty cycle set to 0%
+  sConfigOC.Pulse = pwm_duty_cycle; 
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
@@ -54,7 +54,7 @@ static void MX_TIM1_Init(void)
   sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
   {
-    Error_Handler();  // Call Error_Handler on error
+    Error_Handler(); 
   }
 
   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
@@ -73,14 +73,14 @@ static void MX_TIM1_Init(void)
 
   if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
   {
-    Error_Handler();  // Call Error_Handler on error
+    Error_Handler(); 
   }
 
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
-  // PD2     ------> TIM1_CH1N (PWM output)
+
 
   GPIO_InitStruct.Pin = GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -89,23 +89,20 @@ static void MX_TIM1_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF2_TIM1;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-  // Configure SysTick Timer to call the callback function every 500 ms
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 2);  // 500 ms
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 2); 
 }
 
-// SysTick interrupt handler
 void SysTick_Handler(void)
 {
   static uint32_t counter = 0;
   
   counter++;
 
-  // Toggle the duty cycle every 500 ms
   if (counter >= 500)
   {
     counter = 0;
-    pwm_duty_cycle = (pwm_duty_cycle == 0) ? 50 : 0;  // Toggle between 0% and 50%
-    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwm_duty_cycle);  // Update PWM duty cycle
+    pwm_duty_cycle = (pwm_duty_cycle == 0) ? 50 : 0; 
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwm_duty_cycle); 
   }
 }
 
@@ -114,6 +111,6 @@ void Error_Handler(void)
   __disable_irq();
   while (1)
   {
-    // Loop forever in case of error
+
   }
 }
